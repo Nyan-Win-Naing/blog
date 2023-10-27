02144b346eb6fe79329b0d217cac82bdf8044fe3
Nyan-Win-Naing <nyanwinnaing1922002@gmail.com>
2023-10-28 00:04:04 +0630

    YAML Fron Matter

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -9,8 +9,12 @@
 </head>
 
 <body>
-    <?php foreach($blogs as $blog): ?>
-        <?= $blog; ?>
+    <?php foreach ($blogs as $blog) : ?>
+        <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
+
+        <div>
+            <p><?= $blog->intro; ?></p>
+        </div>
     <?php endforeach; ?>
 </body>
 
