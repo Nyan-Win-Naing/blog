e28bb442af3ffdf1202823ecb1d8289898f09c60
Nyan-Win-Naing <nyanwinnaing1922002@gmail.com>
2023-10-27 08:36:38 +0630

    First Commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/blog">First Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil animi eos aspernatur nobis distinctio veniam repellendus ipsam rerum! Quasi, reiciendis praesentium molestiae at nobis quidem ipsam voluptas repellat delectus itaque. Odit minus natus dolor consectetur ipsam minima maxime animi debitis architecto voluptatem, consequatur saepe soluta et qui, distinctio repellendus aliquam exercitationem. Ad ab illum ut, debitis, harum nostrum a tenetur maxime reprehenderit iure doloribus qui esse quae tempora. Nesciunt commodi excepturi quaerat reiciendis placeat rem, eligendi necessitatibus expedita dignissimos molestias pariatur architecto numquam sapiente deserunt vero obcaecati reprehenderit assumenda, dolorum voluptas a quisquam. Et ratione recusandae, magnam quaerat consectetur eius.</p>
+    </article>
+    <a href="/">go back</a>
+</body>
+
+</html>
